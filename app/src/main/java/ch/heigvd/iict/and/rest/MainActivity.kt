cb912bf807a6f0e3d268a37c006b8f2a6ebc4019
RemyBlr <remy.bleuer@hotmail.com>
2025-12-09 09:47:10 +0100

    Added lab6 files

---
+++ app/src/main/java/ch/heigvd/iict/and/rest/MainActivity.kt
@@ -0,0 +1,72 @@
+package ch.heigvd.iict.and.rest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ch.heigvd.iict.and.rest.databinding.ActivityMainBinding
+import ch.heigvd.iict.and.rest.viewmodels.ContactsViewModel
+import ch.heigvd.iict.and.rest.viewmodels.ContactsViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val contactsViewModel: ContactsViewModel by viewModels {
+        ContactsViewModelFactory((application as ContactsApplication).repository)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // depuis android 15 (sdk 35), le mode edge2edge doit être activé
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // comme edge2edge est activé, l'application doit garder un espace suffisant pour la barre système
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // la barre d'action doit être définie dans le layout, on la lie à l'activité
+        setSupportActionBar(binding.toolbar)
+
+        // contenu
+        binding.mainFabNew.setOnClickListener {
+            // FIXME - create a new contact
+            Toast.makeText(this, "TODO - Création d'un nouveau contact", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_main_synchronize -> {
+                contactsViewModel.refresh()
+                true
+            }
+            R.id.menu_main_populate -> {
+                contactsViewModel.enroll()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+}
---
+++ app/src/main/java/ch/heigvd/iict/and/rest/viewmodels/ContactsViewModel.kt
@@ -0,0 +1,37 @@
+package ch.heigvd.iict.and.rest.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import ch.heigvd.iict.and.rest.ContactsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ContactsViewModel(private val repository: ContactsRepository) : ViewModel() {
+
+    val allContacts = repository.allContacts
+
+    // actions
+    fun enroll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            // TODO
+        }
+    }
+
+    fun refresh() {
+        viewModelScope.launch(Dispatchers.IO) {
+            // TODO
+        }
+    }
+
+}
+
+class ContactsViewModelFactory(private val repository: ContactsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ContactsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ContactsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
